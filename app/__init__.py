96f1f78de2667e5f62edd38078ec4a43cfc15814
StayAway <56220523+reankornation@users.noreply.github.com>
2024-01-28 19:39:53 +0200

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = b"abmobusd"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///file.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import views
